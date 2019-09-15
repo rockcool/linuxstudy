9586b47d7fc5e187f726bcb1c20d916ca60d4cd7
Coolwyser <37362915+Coolwyser@users.noreply.github.com>
2019-09-16 07:44:41 +0800

    This is my 1st commit

--- HelloWrold.c
+++ HelloWrold.c
@@ -1,6 +1,21 @@
-/**/
+/*This is my first file crated by VIM
+ * I just want to show how to modify this and how to save it!
+ * */
 
 main()
 {
+    int this is testing.
+    for(i=0; i < 100; i++)
+    {
+    
+  This is my first VI code ab Line = 0;   xyjjjjjjjjx = 
+    {
+	This is the vi code , just want to make sure it it ok!@
+    //  Test it itself
+    //  e
+    
+
+    }		//  test it 
+
 
 }
